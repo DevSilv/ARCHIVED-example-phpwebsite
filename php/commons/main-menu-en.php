136d63b6021db7d3933e38c74b7ba56e1bd1af6f
silvuss <silvuss@o2.pl>
2018-04-24 03:16:28 +0200

    Fix #4 -  change <a>s' href attribute notation

--- php/commons/main-menu-en.php
+++ php/commons/main-menu-en.php
@@ -1,13 +1,13 @@
 <nav class="main-menu">
     <ul class="main-menu__list">
         <li class="main-menu__list__item">
-            <a href="./index-en.php" class="link main-menu__link main-menu__link--current-page">Home</a>
+            <?php echo '<a href="/php/index-en.php" class="link main-menu__link main-menu__link--current-page">Home</a>'; ?>
         </li>
         <li class="main-menu__list__item">
-            <a href="./websites-en.php" class="link main-menu__link">Websites for front-end developers</a>
+            <?php echo '<a href="/php/websites-en.php" class="link main-menu__link">Websites for front-end developers</a>'; ?>
         </li>
         <li class="main-menu__list__item">
-            <a href="./image-slider-en.php" class="link main-menu__link">A simple image slider</a>
+            <?php echo '<a href="/php/image-slider-en.php" class="link main-menu__link">A simple image slider</a>'; ?>
         </li>
         <li class="main-menu__list__item">
             <a target="_blank" href="https://demotywatory.pl/4254109/To-nie-o-to-chodzizebys-byl-cieplutkimilutki-czy-slodziutki" class="link main-menu__link">To the kingdom of Far Far Away (demotywatory.pl)</a>
